40310a471213ee5ed3222ce5c853e64eff9fe552
Stanislav Kraievskyi <krailastas@gmail.com>
2019-03-06 22:12:36 +0200

    add catalog

--- blacksmiths/urls.py
+++ blacksmiths/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
 
-from content.views import BaseView, BookView, SuccessView
+from content.views import BaseView, BookView, SuccessView, CategoryView
 
 urlpatterns = [
     url(r'^$', BaseView.as_view(), name='home'),
+    url(r'^catalog/(?P<pk>\d+)$', CategoryView.as_view(), name='category'),
     url(r'^book$', BookView.as_view(), name='book'),
     url(r'^success', SuccessView.as_view(), name='success'),
     url(r'^admin/', admin.site.urls)
--- content/views.py
+++ content/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, FormView
+from django.views.generic import TemplateView, FormView, DetailView
 
 from content.forms import BookForm
 from content.models import MainPages, Personal, Galery, Category
@@ -29,3 +29,16 @@ class BookView(FormView):
 
 class SuccessView(TemplateView):
     template_name = 'success.html'
+
+
+class CategoryView(DetailView):
+    template_name = 'catalog.html'
+    model = Category
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['main'] = MainPages.objects.all()[0]
+        context['personal'] = Personal.objects.filter(is_active=True).order_by('order')[:4]
+        context['galery'] = Galery.objects.filter(is_active=True).order_by('order')
+        context['category'] = Category.objects.filter(is_active=True).order_by('order')
+        return context
